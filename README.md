7f80b3dd476b9f6fb6387fd88a54c73af5f32ddf
Maxime EUZIERE <maximz.euziere@gmail.com>
2012-10-27 11:06:55 +0200

    added 2 functions + IE compatibility

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Lazy.js
 =======
 
+The ultra-light, so-useful, cross-browser JS library
+
 Main page: http://xem.github.com/Lazy/
--- lazy.js
+++ lazy.js
@@ -11,15 +11,30 @@ $b = $("body")[0];
 
 // DOM selector
 function $(query){
-  if(query[0] == "#"){
+  if(query.charAt(0) == "#"){
     return $d.getElementById(query.substr(1));
   }
-  else if(query[0] == "."){
-    return $d.getElementsByClassName(query.substr(1));
+  else if(query.charAt(0) == "."){
+    if(document.getElementsByClassName){
+      return $d.getElementsByClassName(query.substr(1));
+    }
+    else{
+      var a = [];
+      var re = new RegExp('(^| )'+query.substr(1)+'( |$)');
+      var els = $("*");
+      for(var i=0,j=els.length; i<j; i++)
+          if(re.test(els[i].className))a.push(els[i]);
+      return a;
+    }
   }
   else{
     return $d.getElementsByTagName(query);
   }
+}
+
+// DOM remover
+function remove(element){
+  element.parentNode.removeChild(element);
 }
 
 // event handlers
@@ -42,11 +57,62 @@ function off(element, event, func){
 }
 
 // CSS getter and setter
+if (!window.getComputedStyle){
+  window.getComputedStyle = function(el, pseudo){
+    this.el = el;
+    this.getPropertyValue = function(prop){
+      var re = /(\-([a-z]){1})/g;
+      if (prop == 'float'){
+        prop = 'styleFloat';
+      }
+      if (re.test(prop)){
+        prop = prop.replace(re, function (){
+          return arguments[2].toUpperCase();
+        });
+      }
+      return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+    }
+    return this;
+  }
+}
+
 function css(element, prop, value){
   if(value){
     element.style[prop] = value;
   }
   else{
     return $w.getComputedStyle(element).getPropertyValue(prop);
+  }
+}
+
+// iterator
+function isNodeList(nodes){
+  var result = Object.prototype.toString.call(nodes);
+  if(result === '[object HTMLCollection]' || result === '[object NodeList]'){
+    return true;
+  }
+  if(typeof(nodes) != 'object'){
+      return false;
+  }
+  if(!('length' in nodes) || !('item' in nodes)){
+    return false;
+  }
+  try{
+    if(nodes(0) === null || (nodes(0) && nodes(0).tagName)) return true;
+  }
+  catch(e){
+    return false;
+  }
+  return false;
+}
+
+function each(array, func){
+  if(isNodeList(array)){
+    for(var i = 0; i < array.length; i++){
+      func.call(array[i]);
+    }
+  }
+  else{
+    func.call(array);
   }
 }
